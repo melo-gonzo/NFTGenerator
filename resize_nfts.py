452c7799925d5ec19860f8b2175cff4e3b44e912
Ljube_Windows <Ljuboslavboskic@gmail.com>
2021-09-14 14:21:11 -0700

    paths for windows but no cv2
    Signed-off-by: Ljube_Windows <Ljuboslavboskic@gmail.com>

--- resize_nfts.py
+++ resize_nfts.py
@@ -3,11 +3,11 @@ import numpy as np
 import cv2
 import os
 
-nft_location = '/home/carmelo/Projects/NFTGenerator/CryptoPoopiesBackup/'
-larger_location = '/home/carmelo/Projects/NFTGenerator/CryptoPoopiesLarger/'
+nft_location = "D:\\Git\\nftgen\\NFTGenerator\\final_images\\"
+larger_location = "D:\\Git\\nftgen\\NFTGenerator\\final_larger\\"
 meta = []
 lips = []
 for poopie in os.listdir(nft_location):
     img = plt.imread(nft_location + poopie)
-    big = cv2.resize(img, (420, 420), fx=0, fy=0, interpolation=cv2.INTER_NEAREST)
-    plt.imsave(larger_location + 'crypto_poopie_' + number + '.png', big)
+    big = cv2.resize(img, (400, 400), fx=0, fy=0, interpolation=cv2.INTER_NEAREST)
+    plt.imsave(larger_location + 'crypto_minecraft_' + number + '.png', big)
